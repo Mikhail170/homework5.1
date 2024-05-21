f4ad479e56d10a92d12b430275adfb1986e05816
Mikhail170 <ga_pe@mail.ru>
2024-05-21 10:28:32 +0200

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,11 @@
+my_list = ['apple', 'orange', 'chery', 'coconut', 'purple']
+print('List: ', my_list)
+print('First element: ', my_list[0])
+print('Last element: ', my_list[-1])
+my_list[2] = 'apricot'
+print('Modified list: ', my_list)
+my_dict = {'table': "стол", 'chair': 'стул', 'sofa': 'диван'}
+print(my_dict)
+print(my_dict.get('table'))
+my_dict['table'] = 'шкаф'
+print(my_dict)
